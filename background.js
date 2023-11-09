c8e82d8a5ede34a88a20947246ab80a4f3d250b9
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-09 23:47:58 +0100

    [WIP]

--- background.js
+++ background.js
@@ -1,23 +1,39 @@
+
+// On setup
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Installed");  
 
   // https://developer.chrome.com/docs/extensions/reference/storage/
   chrome.storage.sync.set({
-      name: "Jack"
+    title: "Mailtrack",
+    welcomeMsg: "Ready to track your emails !",
   });
 
+  chrome.storage.sync.get(["title", "welcomeMsg"], function(result) {
 
-
-  chrome.storage.sync.get("name", function(result) {
     // ID must be different to see the notification, otherwise nothing will happen !
-    chrome.notifications.create(`notif-${new Date().getTime()}`, {
+    chrome.notifications.create(`mailtrack-welcome-${new Date().getTime()}`, {
       type: "basic",
       iconUrl: "icon.png",
-      title: `${result.name}`,
-      message: "Test notification"
+      title: `${result.title}`,
+      message: `${result.welcomeMsg}`
     });
+
   });
-
-
  
 });
+
+
+chrome.tabs.onUpdated.addListener(function(_tabId, changeInfo, tab) {
+  if ( tab.url.includes("mail.google.com/mail/u/0/#inbox?compose=new") && changeInfo.status == 'complete' ) {
+    console.log("Open new email writing")
+  }
+});
+
+
+// On click
+// chrome.browserAction.onClicked.addListener(function(tab) {
+
+//   // https://developer.chrome.com/docs/extensions/reference/tabs/
+//   chrome.tabs.create({ url: "https://mailtrack.io/" });
+
+// });
