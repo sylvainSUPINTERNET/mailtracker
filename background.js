2a45d634693203639db7e9043b07e83bf458887b
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-14 20:01:25 +0100

    [WIP] context form

--- background.js
+++ background.js
@@ -1,59 +1,106 @@
 
-// On setup
+
 chrome.runtime.onInstalled.addListener(() => {
 
-  // https://developer.chrome.com/docs/extensions/reference/storage/
-  chrome.storage.sync.set({
-    title: "Mailtrack",
-    welcomeMsg: "Ready to track your emails !",
+  // add tab to context menu
+  chrome.contextMenus.create({
+    id: "Add gmail pixel",
+    title: "Add gmail pixel",
+    contexts: ["editable"]
   });
 
-  chrome.storage.sync.get(["title", "welcomeMsg"], function(result) {
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "Add gmail pixel") {
 
-    // ID must be different to see the notification, otherwise nothing will happen !
-    chrome.notifications.create(`mailtrack-welcome-${new Date().getTime()}`, {
-      type: "basic",
-      iconUrl: "icon.png",
-      title: `${result.title}`,
-      message: `${result.welcomeMsg}`
-    });
+      chrome.tabs.sendMessage(tab.id, { action: "doSomethingSpecial" });
 
+    }
   });
- 
-});
-
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   
-  let regex = /^https:\/\/mail\.google\.com\/mail\/u\/0\/(.*compose=new)?$/;
 
-  if ( regex.test(tab.url) && changeInfo.status == 'complete' ) {
-    console.log("Open new email writing")
 
-    chrome.scripting.insertCSS({
-      target: { tabId: tabId },
-      files: ['./foreground.css']
-    }).then( () => {
-
-      console.log("CSS injected")
-
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['foreground.js']
-      }); 
-
-      console.log("Script injected")
-
-    }).catch( err => console.log(err)) 
-  }
 
 });
 
 
-// On click
-// chrome.browserAction.onClicked.addListener(function(tab) {
+// // On setup
+// chrome.runtime.onInstalled.addListener(() => {
 
-//   // https://developer.chrome.com/docs/extensions/reference/tabs/
-//   chrome.tabs.create({ url: "https://mailtrack.io/" });
+
+//   chrome.contextMenus.create({
+//     id: "Add gmail pixel",
+//     title: "Add gmail pixel",
+//     contexts: ["editable"]
+//   });
+
+
+//   // https://developer.chrome.com/docs/extensions/reference/storage/
+//   chrome.storage.sync.set({
+//     title: "Mailtrack",
+//     welcomeMsg: "Ready to track your emails !",
+//   });
+
+//   chrome.storage.sync.get(["title", "welcomeMsg"], function(result) {
+
+//     // ID must be different to see the notification, otherwise nothing will happen !
+//     chrome.notifications.create(`mailtrack-welcome-${new Date().getTime()}`, {
+//       type: "basic",
+//       iconUrl: "icon.png",
+//       title: `${result.title}`,
+//       message: `${result.welcomeMsg}`
+//     });
+
+//   });
+ 
+// });
+
+
+// chrome.contextMenus.onClicked.addListener(function(info, tab) {
+//   if (info.menuItemId === "Add gmail pixel") {
+
+//     // TODO: get parent tab id to inject HTML pixel ( should only work in context of a new email and target is content_editable)
+//     // console.log("POGT")
+//     // chrome.tabs.create({}); // This will open a new tab
+//   }
+// });
+
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  
+//   let regex = /^https:\/\/mail\.google\.com\/mail\/u\/0\/(.*compose=new)?$/;
+
+//   if ( regex.test(tab.url) && changeInfo.status == 'complete' ) {
+//     console.log("Open new email writing")
+
+//     chrome.scripting.insertCSS({
+//       target: { tabId: tabId },
+//       files: ['./foreground.css']
+//     }).then( () => {
+
+//       console.log("CSS injected")
+
+//       chrome.scripting.executeScript({
+//         target: { tabId: tabId },
+//         files: ['foreground.js']
+//       }); 
+
+//       console.log("Script injected")
+
+//     }).catch( err => console.log(err)) 
+//   }
 
 // });
+
+
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   console.log("Élément cliqué :", message.element);
+// });
+
+
+// // On click
+// // chrome.browserAction.onClicked.addListener(function(tab) {
+
+// //   // https://developer.chrome.com/docs/extensions/reference/tabs/
+// //   chrome.tabs.create({ url: "https://mailtrack.io/" });
+
+// // });
--- foreground.js
+++ foreground.js
@@ -1,75 +1,95 @@
-// Can't use const / let here !
-let currentTabs = new Set(document.querySelectorAll('div[g_editable]'));
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === "doSomethingSpecial") {
+      // L'action spécifique à réaliser après le clic droit sur l'élément du menu
+      console.log("Action spécifique déclenchée");
+    }
 
-
-document.addEventListener('click', function(event) {
-    if ( currentTabs.has(event.target) ) {
-        console.log("Clicked on a tab !", event.target.id , "Position" , event.clickX, event.clickY);
-    };
 });
 
-
-// Track tabs state
-var targetNode = document.body;
-var observerConfig = { attributes: true, childList: true, subtree: true };
-// yes it's dogshit but it's working with offuscation
-var observer = new MutationObserver( _mutations => {
-    currentTabs = new Set(document.querySelectorAll('div[g_editable]'));
-    // console.log(currentTabs);
-});
-observer.observe(targetNode, observerConfig);
+document.addEventListener('contextmenu', function(event) {
+    let clickedElement = event.target;
+    chrome.runtime.sendMessage({element: clickedElement.outerHTML});
+  }, true);
 
 
+// // Can't use const / let here !
+// let currentTabs = new Set(document.querySelectorAll('div[g_editable]'));
 
 
+// document.addEventListener('contextmenu', function(event) {
+//     let clickedElement = event.target;
+//     chrome.runtime.sendMessage({element: clickedElement.outerHTML});
+//   }, true);
 
 
-
-// 
-// console.log(emailsEditTabs);
-
-// if ( emailsEditTabs ) {
-//     console.log("ah oué")
-// }
-
-
-
-
-// const ce_main_container = document.createElement('DIV');
-// const ce_name = document.createElement('DIV');
-// const ce_input = document.createElement('INPUT');
-// const ce_button = document.createElement('DIV');
-
-// ce_main_container.classList.add('ce_main');
-// ce_name.id = 'ce_name';
-// ce_input.id = 'ce_input';
-// ce_button.id = 'ce_button';
-
-// ce_name.innerHTML = `Hello NAME`;
-// ce_button.innerHTML = `Change name.`;
-
-// ce_main_container.appendChild(ce_name);
-// ce_main_container.appendChild(ce_input);
-// ce_main_container.appendChild(ce_button);
-
-
-// document.querySelector('body').appendChild(ce_main_container);
-
-// chrome.runtime.sendMessage({ 
-//     message: "get_name"
-// }, response => {
-//     if (response.message === 'success') {
-//         ce_name.innerHTML = `Hello ${response.payload}`;
-//     }
+// document.addEventListener('click', function(event) {
+//     if ( currentTabs.has(event.target) ) {
+//         console.log("Clicked on a tab !", event.target.id , "Position" , event.clickX, event.clickY);
+//     };
 // });
 
-// ce_button.addEventListener('click', () => {
-//     chrome.runtime.sendMessage({ 
-//         message: "change_name",
-//         payload: ce_input.value
-//     }, response => {
-//         if (response.message === 'success') {
-//             ce_name.innerHTML = `Hello ${ce_input.value}`;
-//         }
-//     });
+
+// // Track tabs state
+// var targetNode = document.body;
+// var observerConfig = { attributes: true, childList: true, subtree: true };
+// // yes it's dogshit but it's working with offuscation
+// var observer = new MutationObserver( _mutations => {
+//     currentTabs = new Set(document.querySelectorAll('div[g_editable]'));
+//     // console.log(currentTabs);
 // });
+// observer.observe(targetNode, observerConfig);
+
+
+// // /html/body/div[21]/div/div/div/div[1]/div[${variable}]/div[1]/div[1]/div/div/div/div[3]/div/div/div[4]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/div/div/div[2]/div[2]/div/div/div[1]
+
+
+
+
+// // 
+// // console.log(emailsEditTabs);
+
+// // if ( emailsEditTabs ) {
+// //     console.log("ah oué")
+// // }
+
+
+
+
+// // const ce_main_container = document.createElement('DIV');
+// // const ce_name = document.createElement('DIV');
+// // const ce_input = document.createElement('INPUT');
+// // const ce_button = document.createElement('DIV');
+
+// // ce_main_container.classList.add('ce_main');
+// // ce_name.id = 'ce_name';
+// // ce_input.id = 'ce_input';
+// // ce_button.id = 'ce_button';
+
+// // ce_name.innerHTML = `Hello NAME`;
+// // ce_button.innerHTML = `Change name.`;
+
+// // ce_main_container.appendChild(ce_name);
+// // ce_main_container.appendChild(ce_input);
+// // ce_main_container.appendChild(ce_button);
+
+
+// // document.querySelector('body').appendChild(ce_main_container);
+
+// // chrome.runtime.sendMessage({ 
+// //     message: "get_name"
+// // }, response => {
+// //     if (response.message === 'success') {
+// //         ce_name.innerHTML = `Hello ${response.payload}`;
+// //     }
+// // });
+
+// // ce_button.addEventListener('click', () => {
+// //     chrome.runtime.sendMessage({ 
+// //         message: "change_name",
+// //         payload: ce_input.value
+// //     }, response => {
+// //         if (response.message === 'success') {
+// //             ce_name.innerHTML = `Hello ${ce_input.value}`;
+// //         }
+// //     });
+// // });
