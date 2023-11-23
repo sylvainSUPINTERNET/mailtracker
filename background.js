55acff6aced632dd6933dd46a5b3054049c2ae14
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-23 21:29:29 +0100

    [UPDATE]

--- background.js
+++ background.js
@@ -1,28 +1,55 @@
 // User has to click on popup to add pixel for ALL email
 
 chrome.runtime.onInstalled.addListener(() => {
-  // generate unique identifier ( use to generate path ) 
-  
-  // if user send query params his id + path of image = himself ( do nothing on server side )
-  // path + no query params matching = other user ( track on server side )
 
-  // Also means if user uninstall after sending email, he will be tracked as other user !
-  let codeValue = UUIDv4.generate();
+	// generate unique identifier ( use to generate path ) 
 
-  chrome.storage.sync.set({ "code":  codeValue }).then(() => {
-    console.log("Code : " + codeValue);
+	// if user send query params his id + path of image = himself ( do nothing on server side )
+	// path + no query params matching = other user ( track on server side )
+
+	// Also means if user uninstall after sending email, he will be tracked as other user !
+	let codeValue = UUIDv4.generate();
+
+	chrome.storage.sync.set({ "code":  codeValue }).then(() => {
+	console.log("Code : " + codeValue);
+	});
+
+
+	// add tab to context menu
+  chrome.contextMenus.create({
+    id: "addGmailPixel",
+    title: "Add Gmail pixel",
+    contexts: ["editable"]
   });
 
 
-  let injected = false;
-  
+  let elId = null;
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "addGmailPixel") {
+		console.log("STEP 3")
+        console.log("addGmailPixel clicked");
+		console.log("Add pixel to element : ", elId);
+
+		chrome.storage.sync.get(['code'], function(result) {
+			chrome.tabs.sendMessage(tab.id, { action: "addPixelAction", elementId: elId, code: result.code });
+		});
+    }});
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+	if (message.action === "lastClicked") {
+		console.log("STEP 2 ")
+		console.log(message);
+		console.log("Last clicked : ", message.elementId);
+		elId = message.elementId;
+	}
+  })
+
+
   let regex = /^https:\/\/mail\.google\.com\/mail\/u\/0\/(.*compose=new)?$/;
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { 
 	console.log("CHANGE TAB : ", tab.url);
     if ( regex.test(tab.url) && changeInfo.status == 'complete') {  
-		
-		if ( injected ) return;
-		
+			
 		console.log("Open new email writing");
 		chrome.scripting.insertCSS({
 			target: { tabId: tabId },
@@ -33,72 +60,12 @@ chrome.runtime.onInstalled.addListener(() => {
 			target: { tabId: tabId },
 			files: ['foreground.js']
 		});
-		
-	  injected = true;
-
     }
   });
-
 });
 
 
 
-//   // connect(); 
-
-//   // code use to identify the user and avoid to track himself
-//   let codeValue = UUIDv4.generate();
-//   chrome.storage.sync.set({ "code":  codeValue }).then(() => {
-//     console.log("Code : " + codeValue);
-//   });
-  
-
-//   // add tab to context menu
-//   chrome.contextMenus.create({
-//     id: "addGmailPixel",
-//     title: "Add Gmail pixel",
-//     contexts: ["editable"]
-//   });
-
-  
-
-//   // inject foreground
-//   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  
-//     let regex = /^https:\/\/mail\.google\.com\/mail\/u\/0\/(.*compose=new)?$/;
-  
-//     if ( regex.test(tab.url) && changeInfo.status == 'complete' ) {
-//       console.log("Open new email writing")
-  
-//       chrome.scripting.insertCSS({
-//         target: { tabId: tabId },
-//         files: ['./foreground.css']
-//       }).then( () => {
-  
-//         console.log("CSS injected")
-  
-//         chrome.scripting.executeScript({
-//           target: { tabId: tabId },
-//           files: ['foreground.js']
-//         }); 
-  
-//         console.log("Script injected")
-  
-//       }).catch( err => console.log(err)) 
-//     }
-  
-//   });
-
-//   //Select item in menu context
-//   chrome.contextMenus.onClicked.addListener(function(info, tab) {
-//     console.log("Clicked on context menu", info, tab);
-  
-//     if ( info.editable && info.menuItemId === "addGmailPixel" ) {
-//       chrome.tabs.sendMessage(tab.id, { action: "addPixelAction" });
-//     }
-  
-//   });
-  
-// });
 
 var UUIDv4 = new function() {
 	function generateNumber(limit) {
