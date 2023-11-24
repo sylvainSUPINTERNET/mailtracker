c14f2bee196568ca68925c162af8a50bdbfbdbdc
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-24 19:46:13 +0100

    [FDP]

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 // User has to click on popup to add pixel for ALL email
 
-chrome.runtime.onInstalled.addListener(() => {
+importScripts('service-worker-utils.js')
+
+// chrome.runtime.onInstalled.addListener(() => { });
 
 	// generate unique identifier ( use to generate path ) 
 
@@ -62,46 +64,11 @@ chrome.runtime.onInstalled.addListener(() => {
 		});
     }
   });
-});
 
 
 
 
-var UUIDv4 = new function() {
-	function generateNumber(limit) {
-	   var value = limit * Math.random();
-	   return value | 0;
-	}
-	function generateX() {
-		var value = generateNumber(16);
-		return value.toString(16);
-	}
-	function generateXes(count) {
-		var result = '';
-		for(var i = 0; i < count; ++i) {
-			result += generateX();
-		}
-		return result;
-	}
-	function generateVariant() {
-		var value = generateNumber(16);
-		var variant =  (value & 0x3) | 0x8;
-		return variant.toString(16);
-	}
-    // UUID v4
-    //
-    //   varsion: M=4 
-    //   variant: N
-    //   pattern: xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
-    //
-	this.generate = function() {
-  	    var result = generateXes(8)
-  	         + '-' + generateXes(4)
-  	         + '-' + '4' + generateXes(3)
-  	         + '-' + generateVariant() + generateXes(3)
-  	         + '-' + generateXes(12)
-  	    return result;
-	};
-};
+
+
 
 
